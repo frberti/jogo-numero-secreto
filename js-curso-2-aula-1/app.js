2da333601c68e7b9b951ca246f5e842fd8a9de39
Francisco Berti <francisco.berti@zarpsystem.com.br>
2023-12-19 10:20:28 -0300

    Refactor

--- js-curso-2-aula-1/app.js
+++ js-curso-2-aula-1/app.js
@@ -1,6 +1,6 @@
 
 let listaDeNumerosSorteados = [];
-let limiteDeNumeros = 10;
+let limiteDeNumeros = 100;
 let numeroSecreto = geraNumeroAleatorio();
 let tentativas = 1;
 mensagenInicial();
@@ -14,7 +14,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function mensagenInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', `Escolha um número entre 1 e ${limiteDeNumeros}`);
 }
 
 function verificarChute() {
